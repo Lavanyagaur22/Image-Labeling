e7a1c80d9f8eb544c641976f63b2b39ee3b8dbc8
Lavanya Gaur <lavanyagaur22@gmail.com>
2019-11-22 13:51:04 +0530

    set up fritz sdk

--- app/src/main/java/com/lavanya/imagelabeling/MainActivity.kt
+++ app/src/main/java/com/lavanya/imagelabeling/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.lavanya.imagelabeling
 
-import androidx.appcompat.app.AppCompatActivity
+import ai.fritz.core.Fritz
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val API_KEY = "dcf6227d8ecf4968b4e1a1b5fc1c483b"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Fritz.configure(this, API_KEY)
+
     }
 }
